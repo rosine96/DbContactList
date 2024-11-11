e2bf813cb212d7229b6ef20a51a3960ebc534740
rosine ngono <ngrosine07@gmail.com>
2024-11-12 00:49:50 +0100

    first commit

---
+++ Clontact.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*cmd.ExecuteReader(),cmd.ExecuteScalar(),cmd.ExecuteNonQuery()*/
+
+namespace DbContactList
+{
+    public class Contact
+    {
+        public string? name { get; private set; }
+        public string? firstName { get; private set; }
+        public string? phoneNumber { get; private set; }
+
+        public Contact(string name, string firstName, string phoneNumber)
+        {
+            if((name.Length > 10)||(firstName.Length>10)||(phoneNumber.Length>10)) 
+            {
+                throw new ArgumentException("la longueur des elements ne doit pas depasser 10");
+            }
+            if(phoneNumber.All(char.IsDigit)==false)
+                { throw new ArgumentException("le numero doit etre compose uniquement des chiffres"); }
+            
+            this.name = name;
+            this.firstName = firstName;
+            this.phoneNumber = phoneNumber;
+        }
+        public static void GetContact(string name,SqlConnection connection)
+        {
+            string query = $"select * from Contact where nom = @name";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+               
+            {
+                cmd.Parameters.AddWithValue("@name", name);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"nom:{reader["nom"]} prenom:{reader["prenom"]} telephone:{reader["telephone"]}");
+                    }
+                }
+            }
+        }
+        public static void GetAlistOfContact(SqlConnection connection)
+        {
+            string query = "select * from Contact";
+            using(SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                using(SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"nom:{reader["nom"]} prenom:{reader["prenom"]} telephone:{reader["telephone"]}");
+                    }
+                }
+
+            }
+        }
+        public void InsertContact(SqlConnection connection)
+        {
+            string query = "insert into Contact values (@prenom,@nom,@telephone)";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@nom", this.name);
+                cmd.Parameters.AddWithValue("@prenom", this.firstName);
+                cmd.Parameters.AddWithValue("@telephone", this.phoneNumber);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+        public static void DeleteContact(string name,  SqlConnection connection)
+        {
+            string query = "Delete from Contact where nom = @nom";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@nom", name);
+                
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+        public  void UpdateContact(string oldName, SqlConnection connection)
+        {
+            string query = "update Contact set nom=@nom,prenom=@prenom,telephone=@telephone where nom=@ancienNom";
+            using (SqlCommand cmd = new SqlCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@nom", this.name);
+                cmd.Parameters.AddWithValue("@prenom", this.firstName);
+                cmd.Parameters.AddWithValue("@telephone", this.phoneNumber);
+                cmd.Parameters.AddWithValue("@ancienNom", oldName);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data.SqlClient;
+//importer System.Data.SqlClient dans le program
+
+namespace DbContactList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choix = 0,decision=1;
+            string name=null;
+            string firstname = null;
+            string telephoneNumber = null;
+
+            string settings = "Server =DESKTOP-MPQKL6T\\SQLEXPRESS01;Database=ContactList;Integrated Security=True;";
+           
+            using (SqlConnection connection = new SqlConnection(settings))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("connexion reussi");
+                    do
+                    {
+                        Console.WriteLine(@"welcome to contactList!!!what do you want to do?
+                             1-get all of contact
+                             2-get one contact
+                             3-add a contact
+                             4-delete a contact
+                             5-update a contact");
+                        choix = int.Parse(Console.ReadLine());
+                        switch (choix)
+                        {
+                            case 1:
+                                Contact.GetAlistOfContact(connection);
+                                break;
+                            case 2:
+                                Console.WriteLine("entrer le nom");
+                                 name = Console.ReadLine();
+                                Contact.GetContact(name, connection);
+                                break;
+                            case 3:
+                                Console.WriteLine("entrer le prenom");
+                                 firstname = Console.ReadLine();
+                                Console.WriteLine("entrer le nom");
+                                name = Console.ReadLine();
+                                Console.WriteLine("entrer le telephone");
+                                telephoneNumber = Console.ReadLine();
+                                Contact monContact=new Contact(name,firstname, telephoneNumber);
+                                monContact.InsertContact(connection);
+                                Console.WriteLine("ajout reussi");
+                                break;
+                            case 4:
+                                Console.WriteLine("entrer le nom");
+                                name = Console.ReadLine();
+                                Contact.DeleteContact(name, connection);
+                                Console.WriteLine("contact supprime");
+                                break;
+                            case 5:
+                                Console.WriteLine("entrer le nom actuel");
+                                string actName = Console.ReadLine();
+                                Console.WriteLine("entrer le prenom");
+                                firstname = Console.ReadLine();
+                                Console.WriteLine("entrer le nom");
+                                name = Console.ReadLine();
+                                Console.WriteLine("entrer le telephone");
+                                telephoneNumber = Console.ReadLine();
+                                Contact myContact = new Contact(name, firstname, telephoneNumber);
+                                myContact.UpdateContact(actName,connection);
+                                Console.WriteLine("mise a jour complete avec succes");
+                                break;
+                            default:
+                                Console.WriteLine("choix non disponible");
+                                break;
+
+                        }
+                        Console.WriteLine(@"do you want to do anything else?
+                                              1-yes
+                                              2-no");
+                        decision = int.Parse(Console.ReadLine());
+                    } while (decision == 1);
+                    
+                    
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
